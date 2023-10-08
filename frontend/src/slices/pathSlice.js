f53cd1549e462dfbd07edada348d8d7f7f16907b
Panda <pauldalapatt@gmail.com>
2023-10-08 19:43:12 +0530

    Persist path info on reload

--- frontend/src/slices/pathSlice.js
+++ frontend/src/slices/pathSlice.js
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  basePath: "",
-  currentPath: "",
+  basePath: localStorage.getItem('basePath')||"",
+  currentPath: localStorage.getItem('curPath')||"",
   selectionBasePath: "",
   selectionCurrentPath: "",
 };
@@ -13,10 +13,13 @@ const pathSlice = createSlice({
   reducers: {
     setPath: (state, action) => {
       state.currentPath = action.payload;
+      localStorage.setItem('curPath', action.payload)
     },
     setBasePath: (state, action) => {
       state.basePath = action.payload;
       state.currentPath = "";
+      localStorage.setItem('curPath', "")
+      localStorage.setItem('basePath', action.payload)
     },
     resetSelectionPath: (state) => {
       state.selectionBasePath = "";
