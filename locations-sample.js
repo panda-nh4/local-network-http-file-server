ce545b93d2fe7f9c7f0f4d14484c7e70b0362d7e
panda-nh4 <141001374+panda-nh4@users.noreply.github.com>
2023-10-08 22:14:07 +0530

    Update locations-sample.js

--- locations-sample.js
+++ locations-sample.js
@@ -2,5 +2,5 @@ const locations = [
   // { <name_of_location>: "<abs_path_to_location>" }, eg:
   { files: "~/Desktop/temp/" },
 ];
-//Dont forget to rename this file to location.js
+//Dont forget to rename this file to locations.js
 export default locations;
