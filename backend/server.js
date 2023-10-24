88145a6a7f82a093646dbd943267ecbd9092c343
Panda <pauldalapatt@gmail.com>
2023-10-24 17:05:47 +0530

    Eh..

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ dotenv.config();
 import fileRoute from "./Routes/fileRoute.js";
 import dirRoute from "./Routes/dirRoute.js";
 import mediaRoute from "./Routes/mediaRoute.js";
-
+import path from "path";
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
@@ -14,7 +14,12 @@ app.use(cookieParser());
 app.use("/file", fileRoute);
 app.use("/dir", dirRoute);
 app.use("/media", mediaRoute);
-app.get("/", (req, res) => res.send("home"));
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+// app.get("/", (req, res) => res.send("home"));
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`)
 );
